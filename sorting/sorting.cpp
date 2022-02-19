6679df611168d59bf61973a3f515bc1e9b66268c
edgar <edgarmedina1801@gmail.com>
2022-02-19 18:24:28 +0100

    first commit

---
+++ sorting/sorting.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <vector>
+#include <cmath>
+
+class Sorting {
+  private:
+    float running_time = 0;
+  public:
+    int by_insertion(std::vector<int> vector)
+    {
+        return 0;
+    }
+    int by_merge(std::vector<int> vector)
+    {
+        return 0;
+    }
+};
+
+int main() {
+    std::vector<int> reserve_sorted_vector;
+    for( int i = 3; i <= std::pow(2,32); i++ )
+        reserve_sorted_vector.push_back( i );
+
+    Sorting sort;
+    sort.by_insertion(reserve_sorted_vector);
+
+    return 0;
+}
